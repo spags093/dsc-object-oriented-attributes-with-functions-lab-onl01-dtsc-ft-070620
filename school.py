fcc30e46f026620d08657e55c2c1078650b435bf
Jeff <jeff.spags@gmail.com>
2020-09-20 21:19:47 -0400

    Lab Completed

--- school.py
+++ school.py
@@ -1,1 +1,23 @@
-class School:
+class School():
+    
+    def __init__(self, name):
+        self.name = name
+        self.roster = {}
+        
+    def add_student(self, name, grade):
+        if grade in self.roster:
+            self.roster[grade].append(name)
+        else:
+            self.roster[grade] = [name]
+                                      
+    def grade(self, grade):
+        if self.grade:
+            return self.roster[grade]
+        else:
+            return "No students in this grade."
+        
+    def sort_roster(self):
+        new_dict = self.roster
+        for key in new_dict:
+            new_dict[key].sort()
+        return new_dict
